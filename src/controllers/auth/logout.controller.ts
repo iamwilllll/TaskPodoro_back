04f4850ac20fd270e7cab95c2f617777f0e6300a
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-12-07 15:27:52 -0400

    feat(auth): add logout functionality with cookie clearing

---
+++ src/controllers/auth/logout.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { HttpError } from '../../errors/HttpError.js';
+
+export async function logoutController(req: Request, res: Response) {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: true,
+        });
+        res.json({ ok: true, message: 'User logout successful' });
+    } catch (err) {
+        const error = err as HttpError;
+        res.status(error.statusCode).json({
+            ok: false,
+            error: { message: error.message || 'Internal server error' },
+        });
+    }
+}
--- src/routes/auth/authRouter.ts
+++ src/routes/auth/authRouter.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import { registerMiddlewares, verifyUserMiddlewares } from '../../middlewares/index.js';
 import { loginController, registerController, verifyUserController } from '../../controllers/index.js';
 import { loginMiddlewares } from '../../middlewares/auth/login.middlewares.js';
+import { logoutController } from '../../controllers/auth/logout.controller.js';
 
 const authRouter: Router = Router();
 
 //* controllers routes
 authRouter.post('/register', registerMiddlewares, registerController);
 authRouter.post('/verifyUser', verifyUserMiddlewares, verifyUserController);
-authRouter.post('/login',loginMiddlewares, loginController);
+authRouter.post('/login', loginMiddlewares, loginController);
+authRouter.post('/logout', logoutController);
 
 export default authRouter;
