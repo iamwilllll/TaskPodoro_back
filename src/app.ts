3d80f82ae32370d33a18836eaab1039f6ea8acb5
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-12-12 22:59:00 -0400

    Add express.urlencoded middleware to parse urlencoded requests

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import Database from './config/db.js';
 import Server from './config/server.js';
 import appRouter from './routes/appRouter.js';
+
 (async () => {
     await Database.connect();
     await main();
@@ -13,6 +14,7 @@ async function main() {
 
     //* middlewares
     server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
     server.use(cookieParser());
 
     //* routes
