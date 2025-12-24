eb8e7e3be36f1a87aee45997802c9649b990bb67
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2025-12-24 15:16:43 -0400

    Refactor: Move middleware exports to root index.ts

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,9 @@
-export * from './auth/index.js';
-export * from './user/index.js';
+export * from './auth/register.middlewares.js';
+export * from './auth/verifyUser.middlewares.js';
+export * from './auth/login.middlewares.js';
+export * from './auth/forgotPassword.middlewares.js';
+export * from './auth/verifyPassToken.middlewares.js';
+export * from './auth/resetPassword.middlewares.js';
+export * from './auth/verifyToken.middlewares.js';
+
+export * from './user/createGroup.middleware.js';
